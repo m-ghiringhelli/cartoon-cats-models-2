367dc304b374c8cacfd6cef4bf14778c88949bd6
m-ghiringhelli <mghiring@gmail.com>
2022-06-08 16:00:51 -0700

    refactored cats.js to fetch cat class by id, test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,6 +20,7 @@ describe('cats routes', () => {
 
   it('/cats/:id should return cat detail', async () => {
     const res = await request(app).get('/cats/1');
+    console.log('res.body', res.body);
     const felix = {
       id: '1',
       name: 'Felix',
@@ -27,7 +28,7 @@ describe('cats routes', () => {
       url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',
       year: 1892,
       lives: 3,
-      isSidekick: false,
+      is_sidekick: false,
     };
     expect(res.body).toEqual(felix);
   });
